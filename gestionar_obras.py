ff26497e3a7a8a6a7553b59cbc24453447c95e72
AnaliaGF <135098437+AnaliaGF@users.noreply.github.com>
2023-11-17 19:55:00 -0300

    Limpieza Codigo

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -1,55 +1,7 @@
-<<<<<<< HEAD
-from abc import ABC
-from modelo_orm import *
-import pandas as pd
-
-class GestionarObra(ABC):
-
-    df = pd.DataFrame()
-    data = []
-
-    @classmethod
-    def extraer_datos(cls):             
-        cls.df = pd.read_csv("observatorio-de-obras-urbanas.csv").replace('­', '', regex=True)  
-
-    @classmethod
-    def conectar_db(cls):
-        db.initialize(SqliteDatabase("obras_urbanas.db"))
-
-    @classmethod
-    def mapear_orm(cls):
-        db.create_tables([etapa, tipo_obra, tipo_licitacion, area_responsable, barrio, empresa, obra])
-
-    @classmethod
-    def limpiar_datos(cls):
-        cls.data.append(cls.df["entorno"].str.upper().unique())
-        cls.data.append(cls.df["etapa"].str.upper().unique())            
-
-    @classmethod
-    def cargar_datos(cls):
-        pass
-
-    @classmethod
-    def nueva_obra(cls):
-        pass
-
-    @classmethod
-    def obtener_indicadores(cls):
-        pass
-
-
-gestion = GestionarObra()
-
-
-gestion.extraer_datos()
-gestion.limpiar_datos()
-
-=======
 from abc import ABC
 from modelo_orm import *
 from peewee import fn
 import pandas as pd
-
 
 class GestionarObra(ABC):
     df = pd.DataFrame()
@@ -241,4 +193,3 @@ class GestionarObra(ABC):
         print(f"Monto total de contratos: {monto_total_contratos}")
         print(f"Porcentaje promedio de avance: {porcentaje_promedio_avance}")
         print(f"Obra con mayor porcentaje de avance: {obra_mayor_avance.descripcion}")
->>>>>>> 98988099bb8fe4310ca757d2922ae40d23146660
--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-from modelo_orm import *
-
-obranueva = obra(nombre = "kakaka")
-
-print(obranueva.nombre)
-
-
-obranueva.nombre = "Soterramiento tren Sarmiento"
-
-print(obranueva.nombre)
-
-
-obranueva.descripcion = "El tren tiene que pasar por abajo"
-obranueva.beneficiario = "La comunidad en su conjunto"
-
-=======
 from modelo_orm import *
 from gestionar_obras import GestionarObra
 
@@ -40,4 +23,4 @@ for key, value in obra1.__dict__.items():
 print("Información de la primera obra despues de iniciar el proyecto:")
 for key, value in obra2.__dict__.items():
     print(f"{key}: {value}")
->>>>>>> 98988099bb8fe4310ca757d2922ae40d23146660
+
