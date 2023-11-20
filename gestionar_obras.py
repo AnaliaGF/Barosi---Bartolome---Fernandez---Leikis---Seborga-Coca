10140d358cf22e3849e83569f07f9d5948cdac63
AnaliaGF <135098437+AnaliaGF@users.noreply.github.com>
2023-11-20 20:53:25 -0300

    Cambios varios
    Se hicieron los ejercios 4 a 7. Algunos de ellos tienen alternativas.

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -2,15 +2,17 @@ from abc import ABC
 from modelo_orm import *
 from peewee import fn
 import pandas as pd
+import numpy as np
+import math
 
-
-class GestionarObra(ABC):
+class GestionarObra(ABC):    
     df = pd.DataFrame()
     data = []
 
     @classmethod
     def extraer_datos(cls):
-        cls.df = pd.read_csv("observatorio-de-obras-urbanas.csv").replace("", " ", regex=True)
+        cls.df = pd.read_csv("observatorio-de-obras-urbanas.csv")
+        #.replace("", " ", regex=True)
 
     @classmethod
     def conectar_db(cls):
@@ -18,7 +20,7 @@ class GestionarObra(ABC):
 
     @classmethod
     def mapear_orm(cls):
-        db.create_tables([Etapa, Tipo_obra, Tipo_licitacion, Area_responsable, Barrio, Empresa, Obra])
+        db.create_tables([Entorno, Etapa, Tipo_obra, Contratacion_tipo, Area_responsable, Barrio, Empresa, Obra])
 
     @classmethod
     def limpiar_datos(cls):
@@ -34,21 +36,42 @@ class GestionarObra(ABC):
 
         # Manejar valores nulos en otras columnas
         cls.df["descripcion"].fillna("Sin descripcion", inplace=True)
-        cls.df["area_responsable"].fillna("Sin area responsable", inplace=True)
+        cls.df["area_responsable"].fillna("Sin area responsable", inplace=True)      
 
         # Elimina el caracter extraño que se encuentra en algunas palabras acentuadas, reemplaza simbolo por ñ. 
         # Elimina acentos, convierte todo a mayuscula y elimina espacios iniciales y finales de modo de minimizar las repeticiones
         cls.df = cls.df.replace('­', '', regex=True).replace('Ã±', 'ñ', regex=True).replace('á','a', regex=True).replace('é','e', regex=True).replace('í','i', regex=True).replace('ó','o', regex=True).replace('ú', 'u', regex=True).astype(str).apply(lambda col: col.str.upper().str.strip())
 
+        for index, row in cls.df.iterrows():
+          
+            # Reemplaza los caracteres que separan multiples barrios (",", "/", "A" e "Y") por el caracter "|"
+            barriopaso1 = cls.df["barrio"][index].replace(',', '|').replace('/', '|').replace(' A ', '|').replace(' Y ', '|')
+
+            # Crea una lista dividiendo los datos usando el caracter "|" como criterio de division. 
+            barriopaso2  = barriopaso1.split('|')
+            
+            # De la lista creada en barriopaso2 elije el primero elemento (indice 0).
+            # Elimina los espacios adelante y atras.
+            barriopaso3 = barriopaso2[0].strip()
+
+            # Reemplaza el valor original del DataFrame con el obtenido en barriopaso3
+            cls.df["barrio"][index] = barriopaso3
+            
+            # Hace lo mismo que en barrio, pero para comuna en una sola linea
+            cls.df["comuna"][index] = row["comuna"].replace(',', '|').replace('/', '|').replace(' A ', '|').replace(' Y ', '|').split('|')[0].strip()
+
+            # Elimina el simbolo '%' y cambia los '.' por ',' que es el separado de decimales definido.
+            cls.df["porcentaje_avance"][index] = cls.df["porcentaje_avance"][index].replace('%', '').replace(',', '.').strip()
+           
+            try:
+                aux_plazo = math.ceil(float(cls.df["plazo_meses"][index].replace(',', '.').strip()))
+            except ValueError:
+                aux_plazo = None         
+
+            cls.df["plazo_meses"][index] = aux_plazo
+
     @classmethod
     def cargar_datos(cls):
-
-#    try:
-#       User.create(name='Amar', age=20)
-#       transaction.commit()
-#    except DatabaseError:
-#       transaction.rollback()
-   
         
         # Ejecuta los queries envueltos en una transaccion
         with db.atomic() as transaction:     
@@ -58,19 +81,20 @@ class GestionarObra(ABC):
                 # Itera sobre las filas del DataFrame y crea instancias de las clases del modelo ORM
                 for index, row in cls.df.iterrows():
                     # Crea instancias de las clases del modelo ORM con los datos de cada fila
+                    entorno = Entorno.get_or_create(nombre=row["entorno"])
                     etapa = Etapa.get_or_create(nombre=row["etapa"])                
                     tipo_obra = Tipo_obra.get_or_create(nombre=row["tipo"])
                     area_responsable = Area_responsable.get_or_create(nombre=row["area_responsable"])
                     barrio = Barrio.get_or_create(nombre=row["barrio"], comuna=row["comuna"])
                     empresa = Empresa.get_or_create(nombre=row["licitacion_oferta_empresa"], cuit=row["cuit_contratista"])
-                    tipo_licitacion = Tipo_licitacion.get_or_create(nombre=row["contratacion_tipo"])
+                    contratacion_tipo = Contratacion_tipo.get_or_create(nombre=row["contratacion_tipo"])
 
                     obra = Obra.create(
                         nombre=row["nombre"],
-                        entorno=row["entorno"],
+                        id_entorno=entorno[0],
                         id_etapa=etapa[0],
                         id_tipo_obra=tipo_obra[0],
-                        id_tipo_licitacion=tipo_licitacion[0],
+                        id_contratacion_tipo=contratacion_tipo[0],
                         id_area_responsable=area_responsable[0],
                         descripcion=row["descripcion"],
                         monto_contrato=row["monto_contrato"],
@@ -88,6 +112,9 @@ class GestionarObra(ABC):
                         licitacion_anio=row["licitacion_anio"],
                         nro_contratacion=row["nro_contratacion"],
                         mano_obra=row["mano_obra"],
+                        numero_expediente=row["expediente-numero"],
+                        destacada=row["destacada"],
+                        fuente_financiamiento=row["financiamiento"],
                     )
 
                     # Agregar mensaje de impresión
@@ -104,104 +131,114 @@ class GestionarObra(ABC):
 
     @classmethod
     def nueva_obra(cls):
+        
+        # Opcion 1
+        # return Obra()
+
+
+        # Opcion 2
+
         nombre = input("Ingrese el nombre: ")
-        entorno = input("Ingrese el entorno: ")
-        etapa_nombre = input("Ingrese la etapa: ")
-        tipo_obra_nombre = input("Ingrese el tipo de obra: ")
-        tipo_licitacion_nombre = input("Ingrese el tipo de licitación: ")
-        area_responsable_nombre = input("Ingrese el área responsable: ")
-        descripcion = input("Ingrese la descripción: ")
-        monto_contrato = int(input("Ingrese el monto del contrato: "))
-        barrio_nombre = input("Ingrese el nombre del barrio: ")
-        direccion = input("Ingrese la dirección: ")
-        fecha_inicio = input("Ingrese la fecha de inicio (YYYY-MM-DD): ")
-        fecha_fin_inicial = input("Ingrese la fecha de fin inicial (YYYY-MM-DD): ")
-        plazo_meses = int(input("Ingrese el plazo en meses: "))
-        porcentaje_avance = float(input("Ingrese el porcentaje de avance: "))
-        imagen_1 = input("Ingrese la URL de la imagen 1: ")
-        imagen_2 = input("Ingrese la URL de la imagen 2: ")
-        imagen_3 = input("Ingrese la URL de la imagen 3: ")
-        imagen_4 = input("Ingrese la URL de la imagen 4: ")
+        descripcion = input("Ingrese la descripcion: ")
+
+        while True:
+            try:
+                tipo_obra = Tipo_obra.get(nombre=input("Ingrese el tipo de obra: "))
+            except:
+                print("Tipo de Obra inexistente.")
+                continue
+            else:
+                break
+ 
+        while True:
+            try:
+                area_responsable = Area_responsable.get(nombre=input("Ingrese el área responsable: "))
+            except:
+                print("Área responsable inexistente.")
+                continue
+            else:
+                break
+
+        while True:
+            try:
+                barrio = Barrio.get(nombre=input("Ingrese el nombre del barrio: "))
+            except:
+                print("Nombre de barrio inexistente.")
+                continue
+            else:
+                break
         
-        empresa_nombre = input("Ingrese el nombre de la empresa: ")
-        empresa_cuit = input("Ingrese el CUIT de la empresa: ")
+        obra = Obra()
+        obra.nombre = nombre
+        obra.descripcion = descripcion
+        obra.id_tipo_obra = tipo_obra
+        obra.id_area_responsable = area_responsable
+        obra.id_barrio = barrio
+
+        if (obra.nuevo_proyecto() == True):
+            print("Nuevo proyecto de obra creado.")
+            return obra 
+        else:
+            print("No se pudo crear la nueva obra porque faltaron datos obligatorios.")
+            return None
+
+    #@classmethod
+    #def obtener_indicadores(cls):
+
+    #Indicadores que faltan:
+        # a.Listado de todas las áreas responsables
+        # b.Listado de todos los tipos de obra.
+
+        # c.Cantidad de obras que se encuentran en cada etapa:
+        #  obras_por_etapa = (
+        #                     Etapa
+        #                         .select(Etapa.nombre.alias('etapa'), fn.COUNT(Obra.id).alias('cantidad'))
+        #                         .join(Obra, JOIN.LEFT_OUTER)
+        #                         .group_by(Etapa.nombre)
+        #                   ).execute()
+
+        # print(obras_por_etapa)
+
+        # for fila in obras_por_etapa:
+        #     print(fila.etapa, fila.cantidad)
         
-        empresa, _ = Empresa.get(nombre=empresa_nombre, cuit=empresa_cuit)
+    #Más indicadores que faltan
+        # d.Cantidad de obras y monto total de inversión por tipo de obra.
+        # e.Listado de todos los barrios pertenecientes a las comunas 1, 2 y 3.
+        # f.Cantidad de obras finalizadas y su y monto total de inversión en la comuna 1.
+        # g.Cantidad de obras finalizadas en un plazo menor o igual a 24 meses
+        # h.Porcentaje total de obras finalizadas.
+        # i.Cantidad total de mano de obra empleada.
+        # j.Monto total de inversión.
 
-        if(empresa == None):
-            print("La empresa indicada no existe en los registros. Ingrese una nueva empresa.")
-            
-            empresa_nombre = input("Ingrese el nombre de la empresa: ")
-            empresa_cuit = input("Ingrese el CUIT de la empresa: ") 
 
+            # # Contar el número total de obras
+            # total_obras = Obra.select().count()
 
-        licitacion_anio = int(input("Ingrese el año de la licitación: "))
-        nro_contratacion = input("Ingrese el número de contratación: ")
-        mano_obra = input("Ingrese la mano de obra: ")
+            # if total_obras == 0:
+            #     print("No hay obras en la base de datos.")
+            #     return
 
-        etapa, _ = Etapa.get_or_create(nombre=etapa_nombre)
-        tipo_obra, _ = Tipo_obra.get_or_create(nombre=tipo_obra_nombre)
-        tipo_licitacion, _ = Tipo_licitacion.get_or_create(nombre=tipo_licitacion_nombre)
-        area_responsable, _ = Area_responsable.get_or_create(nombre=area_responsable_nombre)
-        barrio, _ = Barrio.get_or_create(nombre=barrio_nombre)
-        
+            # # Agrega estas líneas para imprimir información adicional
+            # obras_con_avance = Obra.select().where(Obra.porcentaje_avance.is_null(False))
 
-        obra = Obra.create(
-            nombre=nombre,
-            entorno=entorno,
-            id_etapa=etapa,
-            id_tipo_obra=tipo_obra,
-            id_tipo_licitacion=tipo_licitacion,
-            id_area_responsable=area_responsable,
-            descripcion=descripcion,
-            monto_contrato=monto_contrato,
-            id_barrio=barrio,
-            direccion=direccion,
-            fecha_inicio=fecha_inicio,
-            fecha_fin_inicial=fecha_fin_inicial,
-            plazo_meses=plazo_meses,
-            porcentaje_avance=porcentaje_avance,
-            imagen_1=imagen_1,
-            imagen_2=imagen_2,
-            imagen_3=imagen_3,
-            imagen_4=imagen_4,
-            id_empresa=empresa,
-            licitacion_anio=licitacion_anio,
-            nro_contratacion=nro_contratacion,
-            mano_obra=mano_obra,
-        )
+            # # Calcular el monto total de los contratos
+            # monto_total_contratos = Obra.select(fn.SUM(Obra.monto_contrato)).scalar()
 
-        print("Nueva obra creada y persistida correctamente.")
+            # # Calcular el porcentaje promedio de avance
+            # porcentaje_promedio_avance = Obra.select(fn.AVG(Obra.porcentaje_avance)).scalar()
 
-        return obra
+            # # Obtener la obra con el mayor porcentaje de avance
+            # obra_mayor_avance = (Obra.select().order_by(Obra.porcentaje_avance.desc()).first())
 
-    @classmethod
-    def obtener_indicadores(cls):
-        # Contar el número total de obras
-        total_obras = Obra.select().count()
+            # print(f"Obras con porcentaje de avance: {obras_con_avance.count()}")                
+            # print(f"Número total de obras: {total_obras}")
+            # print(f"Monto total de contratos: {monto_total_contratos}")
+            # print(f"Porcentaje promedio de avance: {porcentaje_promedio_avance}")
+            # print(f"Obra con mayor porcentaje de avance: {obra_mayor_avance.descripcion}")
 
-        if total_obras == 0:
-            print("No hay obras en la base de datos.")
-            return
-
-        # Agrega estas líneas para imprimir información adicional
-        obras_con_avance = Obra.select().where(Obra.porcentaje_avance.is_null(False))
-        print(f"Obras con porcentaje de avance: {obras_con_avance.count()}")
-
-        # Calcular el monto total de los contratos
-        monto_total_contratos = Obra.select(fn.SUM(Obra.monto_contrato)).scalar()
-
-        # Calcular el porcentaje promedio de avance
-        porcentaje_promedio_avance = Obra.select(
-            fn.AVG(Obra.porcentaje_avance)
-        ).scalar()
-
-        # Obtener la obra con el mayor porcentaje de avance
-        obra_mayor_avance = (
-            Obra.select().order_by(Obra.porcentaje_avance.desc()).first()
-        )
-
-        print(f"Número total de obras: {total_obras}")
-        print(f"Monto total de contratos: {monto_total_contratos}")
-        print(f"Porcentaje promedio de avance: {porcentaje_promedio_avance}")
-        print(f"Obra con mayor porcentaje de avance: {obra_mayor_avance.descripcion}")
+    def chequear_base(cls):
+        if len(db.get_tables()) > 0:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,33 +1,78 @@
-from modelo_orm import *
+#from modelo_orm import *
 from gestionar_obras import GestionarObra
 
 gestion = GestionarObra()
 
 gestion.conectar_db()
-gestion.mapear_orm()
+
+# Ejercicio 17
+#gestion.obtener_indicadores()
+
+#SOLO SE TIENE QUE EJECUTAR LA PRIMERA VEZ
+if (gestion.chequear_base() == False):
+    gestion.mapear_orm()
+    gestion.extraer_datos()
+    gestion.limpiar_datos()
+    gestion.cargar_datos()
+#SOLO SE TIENE QUE EJECUTAR LA PRIMERA VEZ
 
 # Ejercicio 6
 obra1 = gestion.nueva_obra()
 obra2 = gestion.nueva_obra()
 
+
 # Inicio Ejercicio 7
-
 # Ejercicio 8
-
-NuevaObra = Obra()
-
-NuevaObra.id_etapa = 10
-NuevaObra.nombre = "Soterramiento Tren Sarmiento"
-
-NuevaObra.nuevo_proyecto()
-
 obra1.nuevo_proyecto()
 obra2.nuevo_proyecto()
 
-print("Información de la primera obra despues de iniciar el proyecto:")
-for key, value in obra1.__dict__.items():
-    print(f"{key}: {value}")
+# Ejercicio 9
+obra1.iniciar_contratacion()
+obra2.iniciar_contratacion()
+
+# Ejercicio 10
+obra1.adjudicar_obra()
+obra2.adjudicar_obra()
+
+# Ejercicio 11
+obra1.iniciar_obra()
+obra2.iniciar_obra()
+
+# Ejercicio 12
+obra1.actualizar_porcentaje_avance()
+obra2.actualizar_porcentaje_avance()
+
+# Ejercicio 13 (opcional)
+obra1.incrementar_plazo()
+
+# Ejercicio 14 (opcional)
+obra2.incrementar_mano_obra()
+
+# Ejercicio 15
+obra1.finalizar_obra()
+
+# Ejercicio 16
+obra2.rescindir_obra()
+
+
+#NuevaObra = Obra()
+
+# NuevaObra.id_etapa = 10
+# NuevaObra.nombre = "Soterramiento Tren Sarmiento"
+
+# NuevaObra.nuevo_proyecto()
+
+# obra1.nuevo_proyecto()
+# obra2.nuevo_proyecto()
+
+# print("Información de la primera obra despues de iniciar el proyecto:")
+# for key, value in obra1.__dict__.items():
+#     print(f"{key}: {value}")
     
-print("Información de la primera obra despues de iniciar el proyecto:")
-for key, value in obra2.__dict__.items():
-    print(f"{key}: {value}")
+# print("Información de la primera obra despues de iniciar el proyecto:")
+# for key, value in obra2.__dict__.items():
+#     print(f"{key}: {value}")
+
+
+
+
